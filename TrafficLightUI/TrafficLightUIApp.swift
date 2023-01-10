902b5d2d05663b8df75492edc5b885644fb64d49
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-01-10 20:20:58 +0700

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Ярослав Кочкин on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
